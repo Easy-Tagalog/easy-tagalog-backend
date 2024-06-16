f28db0a93d300e07bc758446529cce54cdc4830b
keanu-a <keanucaloua@gmail.com>
2024-06-16 11:45:10 -0700

    Added word serializers and view, TEST

--- words/serializers.py
+++ words/serializers.py
@@ -2,3 +2,38 @@ from rest_framework import serializers
 from .models import Word, Conjugation
 
 # Serializers are for taking a model and converting it to JSON compatible data
+
+
+class ConjugationsListSerializer(serializers.ModelSerializer):
+    """
+    """
+    class Meta:
+        model = Conjugation
+        fields = [
+            'id',
+            'tense',
+            'english',
+            'tagalog',
+            'audio_url',
+            'word'
+        ]
+
+
+class WordsListSerializer(serializers.ModelSerializer):
+    """
+    """
+    conjugations = ConjugationsListSerializer(many=True)
+
+    class Meta:
+        model = Word
+        fields = [
+            'id',
+            'english',
+            'tagalog',
+            'root',
+            'accents',
+            'audio_url',
+            'part_of_speech',
+            'is_irregular_verb',
+            'conjugations'
+        ]
--- words/views.py
+++ words/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
 from .models import Word
 from .serializers import WordSerializer
 
 # These are basically the controllers
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def words_list(request):
+    words = Word.objects.all()
+    serializer = WordSerializer(words, many=True)
+
+    return Response({
+        'data': serializer.data
+    })
